ecebe22300c464c4e76b8b16b8acf6cac7676497
SmoothOperator228 <71877948+SmoothOperator228@users.noreply.github.com>
2021-06-01 12:55:24 +0300

    Delete A&DS_lab3/A&DS2_lab1 directory

--- A&DS_lab3/A&DS2_lab1/src/ListInterface.java
+++
@@ -1,8 +0,0 @@
-public interface ListInterface {
-    public void add(Object obj);
-    public void insert(Object obj, int index);
-    public void remove(int index) throws ObjectNotFoundException;
-    public void replace(int index, Object obj);
-    public void replace(Object changed, Object changer);
-
-}
